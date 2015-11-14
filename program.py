d4fe2900a6fe2f3878818504dc71130826357852
Hernandez, Alex <Alex.Hernandez@inin.com>
2015-11-14 16:29:06 -0500

    docs

--- program.py
+++ program.py
@@ -21,14 +21,14 @@ fullName = raw_input("What is the New Hire's full name: ")
 
 # Store the Job Title
 
-answer = raw_input("What is his/hers job title? \n 1 = Software Engineer \n 2 = Technical Recruiter "
+jobTitle = raw_input("What is his/hers job title? \n 1 = Software Engineer \n 2 = Technical Recruiter "
                    "\n 3 = Platform Test Engineer \n 4 = Health Administrator \n > ")
 
 print "\n"
 
 # Generate the Employee Credentials
 
-employee.generateCredentials(fullName, answer)
+employee.generateCredentials(fullName, jobTitle)
 
 # Call the restart function
 
